fc3610f482452f551630d1396846993da9396134
Jared1989 <39254958+Jared1989@users.noreply.github.com>
2018-06-24 18:04:35 -0400

    Update test_MyBaseball7.py
    Added 3 additional tests

--- testing/test_MyBaseball7.py
+++ testing/test_MyBaseball7.py
@@ -16,3 +16,20 @@ def test_location_time():
     Team2 = "Rays"
     result = location_time(value1, value2, item1a, item1b, item2a, item2b, Team1, Team2)
     assert result == "Rays"
+
+def test_get_teams():
+    request_address = "https://www.espn.com/mlb/standings"
+    result = get_teams(request_address)
+    assert len(result) == 30
+
+def test_get_stats1():
+    request_address = "https://www.espn.com/mlb/standings"
+    headings = ['W', 'L', 'PCT', 'GB', 'HOME', 'AWAY', 'RS', 'RA', 'DIFF', 'STRK', 'L10']
+    result = get_stats(request_address, headings)
+    assert len(result) == 30
+
+def test_get_stats2():
+    request_address = "https://www.espn.com/mlb/standings/_/view/expanded"
+    headings = ['W', 'L', 'PCT', 'GB', 'DAY', 'NIGHT', '1-RUN', 'XTRA', 'ExWL']
+    result = get_stats(request_address, headings)
+    assert len(result) == 30
